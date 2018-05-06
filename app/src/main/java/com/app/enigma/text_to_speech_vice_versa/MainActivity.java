7a4c069422be6efaed53be10aee8f2cb79682fe1
Coderguy321 <vishwasgarg14@gmail.com>
2018-05-06 12:45:44 +0530

    complete repo

---
+++ app/src/main/java/com/app/enigma/text_to_speech_vice_versa/MainActivity.java
@@ -0,0 +1,112 @@
+package com.app.enigma.text_to_speech_vice_versa;
+
+
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity implements
+        TextToSpeech.OnInitListener {
+    /** Called when the activity is first created. */
+
+    private TextToSpeech tts;
+    private Button btnSpeak;
+    private EditText txtText;
+    private  Button stt;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tts = new TextToSpeech(this, this);
+
+        btnSpeak = (Button) findViewById(R.id.btnSpeak);
+        stt = (Button) findViewById(R.id.stt);
+
+        txtText = (EditText) findViewById(R.id.txtText);
+        txtText.setTextColor(ContextCompat.getColor(this, R.color.colorText));
+
+        // button on click event
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                speakOut();
+            }
+
+        });
+
+        stt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                nextActivity();
+            }
+
+        });
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+        // TODO Auto-generated method stub
+
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.US);
+
+            // tts.setPitch(5); // set pitch level
+
+            // tts.setSpeechRate(2); // set speech speed rate
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "Language is not supported");
+            } else {
+                btnSpeak.setEnabled(true);
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed");
+        }
+
+    }
+
+    private void speakOut() {
+
+        String text = txtText.getText().toString();
+
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    private void nextActivity() {
+        Intent activityChangeIntent = new Intent(this, SpeechToTextActivity.class);
+
+        // currentContext.startActivity(activityChangeIntent);
+
+        this.startActivity(activityChangeIntent);
+    }
+
+
+}
